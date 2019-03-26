8dd7778828e7736cfc5b641b6c2bec8702d88cdc
SifatJahan <sifatjahan309@gmail.com>
2019-03-26 19:05:14 +0600

    login signup

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def user_login(request):
+    context = {}
+    next = request.GET.get('next')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            if next:
+                return redirect(next)
+            else:
+                messages.success(request, "You have successfully logged in!")
+                return redirect('finance_management:account')
+        else:
+            messages.error(request, "Provide valid credentials.")
+            return render(request, 'auth/login.html')
+    
+    else:
+        return render(request, 'auth/login.html', context)
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def user_logout(request):
+    messages.success(request, "You have been logged out!")
+    logout(request)
+    return redirect('base:login')
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {})
